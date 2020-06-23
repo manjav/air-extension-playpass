397b4128e785ab03bab204d1b7e86158d0194b65
Mansour <mansurjavadi@gmail.com>
2020-06-23 21:16:46 +0430

    Add android log

--- playpass_android/app/src/main/java/com/gerantech/extensions/functions/LicenceCheckFunction.java
+++ playpass_android/app/src/main/java/com/gerantech/extensions/functions/LicenceCheckFunction.java
@@ -1,10 +1,12 @@
 package com.gerantech.extensions.functions;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
+import com.gerantech.extensions.PlayPassExtension;
 import com.gerantech.extensions.PlayPassExtensionContext;
 
 public class LicenceCheckFunction implements FREFunction
@@ -16,6 +18,7 @@ public class LicenceCheckFunction implements FREFunction
 		Activity a = divExtContext.getActivity();
 //		TelephonyManager manager = (TelephonyManager) a.getSystemService(Context.TELEPHONY_SERVICE);
 //        manager.listen(new CustomPhoneStateListener(a.getApplicationContext(), divExtContext), PhoneStateListener.LISTEN_CALL_STATE);
+		Log.w(PlayPassExtension.LOG_TAG, "LicenceCheckFunction called.");
 
         return null;
 	}
