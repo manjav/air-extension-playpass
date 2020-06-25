8d7d0b9d3bab2609d05be2c9f14a73e9cc08c966
Mansour <mansurjavadi@gmail.com>
2020-06-25 20:53:01 +0430

    Add client-api

--- package/package.bat
+++ package/package.bat
@@ -3,4 +3,4 @@ echo f | xcopy /f /y ..\playpass_as3\bin\playpass_as3_lib.swc
 echo f | xcopy /f /y default\library.swf android\
 echo f | xcopy /f /y ..\playpass_android\app\build\intermediates\packaged-classes\release\classes.jar android\
 
-adt -package -storetype pkcs12 -keystore cert.p12 -storepass ****** -target ane "com.gerantech.extensions.playpass.ane" extension.xml -swc playpass_as3_lib.swc -platform Android-ARM -C android . -platform Android-ARM64 -C android . -platform Android-x86 -C android . -platform default -C default . && echo f | xcopy /f /y com.gerantech.extensions.playpass.ane ..\playpass_test\exts\
+adt -package -storetype pkcs12 -keystore cert.p12 -storepass ****** -target ane "com.gerantech.extensions.playpass.ane" extension.xml -swc playpass_as3_lib.swc -platform Android-ARM -platformoptions platform.xml -C android . -platform Android-ARM64 -platformoptions platform.xml -C android . -platform Android-x86 -platformoptions platform.xml -C android . -platform default -C default . && echo f | xcopy /f /y com.gerantech.extensions.playpass.ane ..\playpass_test\exts\
